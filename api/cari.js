0b41a091945d2cc66546c84944a42f7914c69117
pcc332233-stack <pcc332233@gmail.com>
2026-07-07 13:32:13 +0700

    Implement YouTube search functionality in cari.js
    Refactor cari.js to perform YouTube search and return videos.

--- api/cari.js
+++ api/cari.js
@@ -1,3 +1,12 @@
-module.exports = (req, res) => {
-  res.status(200).send("SERVER AKTIF!");
+const ytSearch = require('yt-search');
+
+module.exports = async (req, res) => {
+    try {
+        const query = req.query.q || 'panda';
+        const result = await ytSearch(query);
+        const videos = result.videos.slice(0, 10);
+        res.status(200).json(videos);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
 };
