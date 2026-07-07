96b4fee3ab7d87ba744e9c9ed7f5195a54b2dc9b
pcc332233-stack <pcc332233@gmail.com>
2026-07-07 13:30:32 +0700

    Replace ytSearch with server active response
    Removed YouTube search functionality and added a simple server active response.

--- api/cari.js
+++ api/cari.js
@@ -1,24 +1,3 @@
-const ytSearch = require('yt-search');
-
-module.exports = async (req, res) => {
-    const query = req.query.q;
-    
-    // Jika tidak ada kata kunci, beri tahu user
-    if (!query) {
-        return res.status(400).json({ error: "Gunakan ?q=kata_kunci" });
-    }
-
-    try {
-        const result = await ytSearch(query);
-        // Pastikan kita mengambil data video saja
-        const videos = result.videos;
-        
-        if (videos && videos.length > 0) {
-            res.status(200).json(videos.slice(0, 5));
-        } else {
-            res.status(200).json({ message: "Tidak ada video ditemukan" });
-        }
-    } catch (err) {
-        res.status(500).json({ error: err.message });
-    }
+module.exports = (req, res) => {
+  res.status(200).send("SERVER AKTIF!");
 };
